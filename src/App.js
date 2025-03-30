8bc931e97493d5e731794ea5eb387ada428b08d9
m-s-sat <mrinalsatyarthi2005@gmail.com>
2025-03-31 00:07:52 +0530

    commit-update

--- src/App.js
+++ src/App.js
@@ -1,73 +1,89 @@
 import Submitfield from './components/Submitfield';
 import Items from './components/Items';
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
 import DataDB from './components/data'
 import './App.css';
 
 function App() {
-  const [data,setData] = useState(DataDB);
+  const [data,dispatch] = useReducer(dataReducer,DataDB);
   const [count,setCount] = useState(0);
   const [editableData, setEditableData] = useState(null);
   const [ID,setID] = useState(-1);
+  function dataReducer(data,action){
+    const newData = [...data];
+    switch(action.type){
+      case 'ADD':
+        return [...data,{id:data.length+1,content:action.payload1,done:action.payload2}];
+      case 'DEL':
+        const filteredData = data.filter((datas)=>datas.id!==action.payload);
+        filteredData.forEach((datas,i)=>{
+          datas.id = i+1;
+        })
+        return filteredData; 
+      case 'UP':
+        const id = action.payload;
+        if(id>1){
+          [newData[id-1],newData[id-2]] = [newData[id-2],newData[id-1]];
+          newData[id-1].id = id;
+          newData[id-2].id = id-1;
+        }
+        newData.forEach((data,i)=>{
+          data.id = i+1;
+        })
+        return [...newData];
+      case 'DW':
+        const id2 = action.payload;
+        if(id2<newData.length){
+          [newData[id2-1],newData[id2]] = [newData[id2],newData[id2-1]];
+          newData[id2-1].id = id2;
+          newData[id2].id = id2+1;
+        }
+        newData.forEach((data,i)=>{
+          data.id = i+1;
+        })
+        return [...newData];
+      case 'ACI':
+        newData[action.payload-1].done = true;
+        return [...newData];
+      case 'DCI':
+        newData[action.payload-1].done = false;
+        return [...newData];
+      case 'UPD':
+        newData[action.id-1].content = action.payload;
+        return newData;
+      default:
+        return data;
+    }
+  }
   function dataset(content,done){
-    setData([...data, {id:data.length+1, content, done}]);
+    dispatch({type:'ADD',payload1:content,payload2:done});
   }
   function deleteToDo(id,done){
-    const newData = data.filter((datas)=>datas.id!==id);
-    newData.forEach((data,i)=>{
-      data.id = i+1;
-    })
     if(done){
       setCount(count-1);
     }
-    setData(newData);
+    dispatch({type:'DEL',payload:id})
   }
   function upBtn(id){
-    const newData = [...data];
-    if(id>1){
-      [newData[id-1],newData[id-2]] = [newData[id-2],newData[id-1]];
-      newData[id-1].id = id;
-      newData[id-2].id = id-1;
-    }
-    newData.forEach((data,i)=>{
-      data.id = i+1;
-    })
-    setData([...newData]);
+    dispatch({type:'UP',payload:id});
   }
   function downBtn(id){
-    const newData = [...data];
-    if(id<newData.length){
-      [newData[id-1],newData[id]] = [newData[id],newData[id-1]];
-      newData[id-1].id = id;
-      newData[id].id = id+1;
-    }
-    newData.forEach((data,i)=>{
-      data.id = i+1;
-    })
-    console.log(newData);
-    setData([...newData]);
+    dispatch({type:'DW',payload:id});
   }
   function add_completed_item(id){
-    const newData = [...data];
-    newData[id-1].done = true;
-    setData([...newData]);
     setCount(count+1);
+    dispatch({type:'ACI',payload:id});
   }
   function subtract_completed_item2(id){
-    const newData = [...data];
-    data[id-1].done = false;
-    setData(newData);
     setCount(count-1);
+    dispatch({type:'DCI',payload:id});
   }
   function update(id){
     setID(id);
     setEditableData(data[id-1]);
   }
   function updateData(datas){
-    const newData = [...data];
-    newData[ID-1].content = datas;
-    setData(newData);
-    console.log(localStorage);
+    dispatch({type:'UPD',payload:datas,id:ID});
   }
   return (
     <div className="App">
